f5cd79779a12356764d28786e2c3e4d18433ca74
fippli <filip.carl.johansson@gmail.com>
2019-03-12 21:16:07 +0100

    patch

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ Parse url strings for embedding youtube videos
 const getVideoId = (url) => {
   const start = url.indexOf("watch?v=") + 8;
   const end = url.indexOf("&");
+  if (end === -1) {
+    return url.substring(start);
+  }
   return url.substring(start, end);
 }
 
